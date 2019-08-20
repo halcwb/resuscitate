b48420ea56afaab871f969da7ad4dc2ea21cd39f
Casper Bollen <halcwb@gmail.com>
2019-08-21 00:30:47 +0200

    chore: added docker settings

--- build.fsx
+++ build.fsx
@@ -123,8 +123,8 @@ Target.create "Bundle" (fun _ ->
     Shell.copyDir publicDir clientDeployPath FileFilter.allFiles
 )
 
-let dockerUser = "safe-template"
-let dockerImageName = "safe-template"
+let dockerUser = "halcwb"
+let dockerImageName = "genapls"
 let dockerFullName = sprintf "%s/%s" dockerUser dockerImageName
 
 Target.create "Docker" (fun _ ->
